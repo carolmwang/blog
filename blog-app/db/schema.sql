8d8abd8ef8245058dc84e3784adab3e2da36458f
Carol Wang <carolmwang@gmail.com>
2018-12-03 15:48:33 -0500

    created schema file with tables

--- blog-app/db/schema.sql
+++ blog-app/db/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users, posts, comments, images, videos;
+DROP TABLE IF EXISTS comments, images, videos, users, posts;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -10,7 +10,42 @@ CREATE TABLE users (
   email VARCHAR(250),
   facebook TEXT,
   instagram TEXT,
-  twitter VARCHAR(300),
+  github TEXT,
+  linkedin TEXT,
+  twitter TEXT,
+  zipcode INTEGER NOT NULL,
   password_digest VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(250),
+  content TEXT,
+  category VARCHAR(250),
+  user_id INTEGER REFERENCES user (id),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  content TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  post_id INTEGER REFERENCES post (id),
+  user_id INTEGER REFERENCES user (id)
+);
+
+CREATE TABLE images (
+  id SERIAL PRIMARY KEY,
+  img_url TEXT,
+  post_id INTEGER REFERENCES post (id),
+  user_id INTEGER REFERENCES user (id)
+);
+
+CREATE TABLE videos (
+  id SERIAL PRIMARY KEY,
+  video_url TEXT,
+  post_id INTEGER REFERENCES post (id),
+  user_id INTEGER REFERENCES user (id)
+);
+
